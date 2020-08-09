cb0d25d889c3a3fe7ff6d4a0410d494b42cc0ac3
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-08-10 00:34:13 +0900

    styling-with-styled-components

--- ex06/styling-with-styled-components/src/components/Button.js
+++ ex06/styling-with-styled-components/src/components/Button.js
@@ -1,5 +1,68 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
+
+const colorStyles = css`
+    ${({ theme, color }) => {
+        const selected = theme.palette[color];
+        return css`
+            background: ${selected};
+            &:hover {
+                background: ${lighten(0.1, selected)};
+            }
+            &:active {
+                background: ${darken(0.1, selected)};
+            }
+            ${(props) =>
+                props.outline &&
+                css`
+                    color: ${selected};
+                    background: none;
+                    border: 1px solid ${selected};
+                    &:hover {
+                        background: ${selected};
+                        color: white;
+                    }
+                `}
+        `;
+    }}
+`;
+
+const sizes = {
+    large: {
+        height: '3rem',
+        fontSize: '1.25rem',
+    },
+    medium: {
+        height: '2.25rem',
+        fontSize: '1rem',
+    },
+    small: {
+        height: '1.75rem',
+        fontSize: '0.875rem',
+    },
+};
+
+const sizeStyles = css`
+    ${({ size }) =>
+        css`
+            height: ${sizes[size].height};
+            font-size: ${sizes[size].fontSize};
+        `}
+`;
+
+const fullWidthStyle = css`
+    ${(props) =>
+        props.fullWidth &&
+        css`
+            width: 100%;
+            justify-content: center;
+            & + & {
+                margin-left: 0;
+                margin-top: 1rem;
+            }
+        `}
+`;
 
 const StyledButton = styled.button`
     display: inline-flex;
@@ -12,24 +75,28 @@ const StyledButton = styled.button`
     padding-left: 1rem;
     padding-right: 1rem;
 
-    height: 2.25rem;
-    font-size: 1rem;
+    ${sizeStyles}
 
-    background: #228be6;
-    &:hover {
-        background: #339af0;
-    }
-    &:active {
-        background: #1c7ed6;
-    }
+    ${colorStyles}
 
     & + & {
         margin-left: 1rem;
     }
+
+    ${fullWidthStyle}
 `;
 
-function Button({ children, ...rest }) {
-    return <StyledButton {...rest}>{children}</StyledButton>;
+function Button({ children, color, ...rest }) {
+    return (
+        <StyledButton color={color} {...rest}>
+            {children}
+        </StyledButton>
+    );
 }
+
+Button.defaultProps = {
+    color: 'blue',
+    size: 'medium',
+};
 
 export default Button;
