8c8eba6320bdc2b55be41afea0e2c009b8fceac1
ansuvin <50893537+ansuvin@users.noreply.github.com>
2020-08-10 01:30:49 +0900

    component_complete

---
+++ ex07/mashup-todolist/src/components/TodoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+import TodoItem from './TodoItem';
+
+const TodoListBlock = styled.div`
+    flex: 1;
+    padding: 20px 32px;
+    padding-bottom: 48px;
+    overflow-y: auto;
+`;
+
+function TodoList() {
+    return (
+        <TodoListBlock>
+            <TodoItem text="프로젝트 생성하기" done={true} />
+            <TodoItem text="프로젝트 생성하기2" done={true} />
+            <TodoItem text="프로젝트 생성하기3" done={false} />
+            <TodoItem text="프로젝트 생성하기4" done={false} />
+        </TodoListBlock>
+    );
+}
+
+export default TodoList;
